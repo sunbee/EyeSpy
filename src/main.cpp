5a9e7b5bbefb6d5c4fa7ff605d8f44395dd58812
sunbee <sanjay.bhatikar@gmail.com>
2022-10-22 23:29:38 -0500

    Added MQTT broker 4 pubsub ERR

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,25 @@
 #include <WiFi.h>
 #include "esp_camera.h"
 #include <base64.h>
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+
+WiFiClientSecure HTTPClient;
+PubSubClient MQTTClient(HTTPClient);
 
 #define deBuG false
 
 #define CAMERA_MODEL_WROVER_KIT
-
 #include "camera_pins.h"
 
 #include "config.h"
-const char* _SSID   = SSID;
-const char* _PWD    = PWD;
+const char* _SSID       = SSID;
+const char* _PWD        = PWD;
+const char* _HiveBroker = HIVEMQ_BROKER; // replace with your broker url
+const char* _HiveUID    = HIVEMQ_UID;
+const char* _HivePWD    = HIVEMQ_PWD;
+const int   _HivePort   = 8883;
+const char* _HiveTopic  = "bluebird";
 
 camera_config_t config;
 
@@ -25,7 +34,43 @@ unsigned long delta = 1000;
 uint8_t*  _jpg_buf = NULL;
 size_t    _jpg_len = 0;
 
+void onmessage(char*, byte*, unsigned int);
+void publish_me(String);
+void reconnect();
 String capturePhoto(uint8_t*, size_t*);
+static const char *root_ca PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
 
 void setup() {
   // put your setup code here, to run once:
@@ -50,6 +95,7 @@ void setup() {
   s->set_brightness(s, 1);  // Increment brightness level by a tick
   s->set_saturation(s, -1); // Decrement saturation level by a tick
 
+  // WiFi
   WiFi.begin(SSID, PWD);
   Serial.print("Making WiFi connection.");
   while (WiFi.isConnected() != true) {
@@ -57,6 +103,12 @@ void setup() {
     delay(500);
   }
   Serial.println("Done!");
+
+  // MQTT
+  // HTTPClient.setInsecure();
+  HTTPClient.setCACert(root_ca);
+  MQTTClient.setServer(_HiveBroker, _HivePort);  // MQTT_IP, 1883 or HiveNQ, 1883
+  MQTTClient.setCallback(onmessage);    
 
   if (deBuG) {
     startCameraServer();
@@ -68,8 +120,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  ;
-
   toc = millis();
   if (toc - tic >= delta) {
     tic = toc;
@@ -81,21 +131,22 @@ void loop() {
       // Encode in base64
       Serial.println(b64enc);
 
-      // Ship to MQTT broker - MacBook
+      // TODO [Alt.]: Ship to MQTT broker - MacBook
 
       // Ship to MQTT broker - HiveMQ
+      if (!MQTTClient.connected()) {
+        Serial.println("Made no MQTT connection.");
+        reconnect();
+      } else {
+        publish_me((String)"Namaste ji!"); // Publisher action
+      }
+      MQTTClient.loop(); // Callbacks handled in event loop
 
-      // Send Telegram notification
-    }
-
-    // Check motion-sensor signal
-    /*
-    * TODO: Integrate motion-sensor
-    * 
-    */
-
-  }
-}
+      // TODO: Send Telegram notification
+    } // end IF deBuG
+    // TODO: Check motion-sensor signal
+  }   // end TIC-TOC
+}     // end LOOP
 
 void config_init() {
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -156,4 +207,45 @@ String capturePhoto(uint8_t* _jpg, size_t* _len) {
     frame = NULL;
   }   // end IF frame .. picture taken
   return b64enc;
+}
+
+void onmessage(char* topic, byte* payload, unsigned int length) {
+  /*
+  Handle a new message published to the subscribed topic on the 
+  MQTT broker and show in the OLED display.
+  This is the heart of the subscriber, making it so the nodemcu
+  can act upon information, say, to operate a solenoid valve when
+  the mositure sensor indicates dry soil.
+  */
+  Serial.print("Got a message in topic ");
+  Serial.println(topic);
+  Serial.print("Received data: ");
+  char message2display[length];
+  for (unsigned int i = 0; i < length; i++) {
+    Serial.print(char(payload[i]));
+    message2display[i] = payload[i];
+  }
+  Serial.println();
+}
+
+void publish_me(String message) {
+  boolean pstatus = MQTTClient.publish_P(_HiveTopic, (const uint8_t*)message.c_str(), message.length(), true);
+  Serial.println(String(pstatus ? "Published" : "ERR") );
+}
+
+void reconnect() {
+  while (!MQTTClient.connected()) {   // Until connected..
+    Serial.print("Connecting to MQTT broker .. ");
+    String clientID = "BIRD_IS_WORD";
+    if (MQTTClient.connect(clientID.c_str(), _HiveUID, _HivePWD)) { // Present credentials
+      Serial.println("connected");
+      MQTTClient.publish(_HiveTopic, "BIRD IS THE WORD");           // Announce status
+      MQTTClient.subscribe(_HiveTopic);                             // Resubscribe
+    } else {
+      Serial.print("failed, rc = ");
+      Serial.print(MQTTClient.state());
+      Serial.println(" try again in 5 seconds.");                   // Retry after delay
+      delay(5000);
+    }
+  }
 }
